6f2f2cb006af4214b35ebb86b851322736768dff
Ilona <ilona.j5@wp.pl>
2020-04-26 16:39:48 +0200

    Login and logout

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -22,4 +22,13 @@ export class NavComponent implements OnInit {
       console.log('Failed to login');
     });
   }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Loged out');
+  }
 }
