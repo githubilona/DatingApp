555c38b6b5c032baf2bf0b96edc2c8330d408089
Ilona <ilona.j5@wp.pl>
2020-04-26 14:42:06 +0200

    Turn back to section 3

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -43,7 +43,6 @@ namespace DatingApp.API.Controllers
         //     return StatusCode(201);
         // }
 
-
         [HttpPost("{login}")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
