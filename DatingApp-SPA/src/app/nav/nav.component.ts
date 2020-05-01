0b300f953608f1a6cb61329d5e093e8562a3fd04
Ilona <ilona.j5@wp.pl>
2020-05-01 16:18:35 +0200

    Get username from token and display it on navbar

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -25,7 +25,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn(){
-    this.authService.loggedIn();
+    return this.authService.loggedIn();
   }
   logout(){
     localStorage.removeItem('token');
