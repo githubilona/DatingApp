87a980e4394daf6713b98ae0c8974a3844699f74
Ilona <ilona.j5@wp.pl>
2020-05-01 15:47:52 +0200

    JWT token lib to improve token handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -25,8 +25,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;
+    this.authService.loggedIn();
   }
   logout(){
     localStorage.removeItem('token');
