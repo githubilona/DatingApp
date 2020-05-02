2a289e739aa1a0f0be321a7976cb8aefae7d8704
Ilona <ilona.j5@wp.pl>
2020-05-02 23:43:10 +0200

    Using Route Resolvers to retrieve data

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -19,6 +19,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-details.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 
 export function tokenGetter(){
@@ -53,7 +55,10 @@ export function tokenGetter(){
       })
    ],
    providers: [
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver
+
    ],
    bootstrap: [
       AppComponent
