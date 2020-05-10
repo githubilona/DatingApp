9cfcfdf759594017c6f57db5fa992dcff9376800
Ilona <ilona.j5@wp.pl>
2020-05-10 17:43:01 +0200

    Actions Filters - last time active

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -55,8 +55,9 @@ namespace DatingApp.API
                             ValidateIssuer =false,
                             ValidateAudience = false
                     };
-
+                    
                 });
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
