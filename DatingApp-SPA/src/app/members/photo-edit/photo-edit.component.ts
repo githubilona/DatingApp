99ed4e59c558587f9a0a2e462d5454434b4420a9
Ilona <ilona.j5@wp.pl>
2020-05-10 16:56:50 +0200

    Fixing the Photos issue for newly registered users

--- DatingApp-SPA/src/app/members/photo-edit/photo-edit.component.ts
+++ DatingApp-SPA/src/app/members/photo-edit/photo-edit.component.ts
@@ -50,6 +50,11 @@ export class PhotoEditComponent implements OnInit {
           isApproved: res.isApproved
         };
         this.photos.push(photo);
+        if (photo.isMain){
+          this.authService.changeMemberPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+        }
       }
     };
   }
