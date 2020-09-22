f114945f0c578f5f36cf46aab867831e98959262
Ilona <ilona.j5@wp.pl>
2020-09-22 15:16:12 +0200

    Filtering by age and gender frontend

--- DatingApp-SPA/src/app/members/memeber-list/memeber-list.component.ts
+++ DatingApp-SPA/src/app/members/memeber-list/memeber-list.component.ts
@@ -12,6 +12,12 @@ import { PaginatedResult, Pagination } from 'src/app/_models/pagination';
 })
 export class MemeberListComponent implements OnInit {
   users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' }
+  ];
+  userParams: any = {};
   pagination: Pagination;
   constructor(private userService: UserService, private alertify: AlertifyService,
               private route: ActivatedRoute) { }
@@ -22,14 +28,23 @@ export class MemeberListComponent implements OnInit {
       this.users = data['users'].result;
       this.pagination = data['users'].pagination;
     });
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
   }
 
   pageChanged(event: any): void{
     this.pagination.currentPage = event.page;
     this.loadUsers();
   }
+  resetFilters(){
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
   loadUsers(){
-    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
     .subscribe((res: PaginatedResult<User[]>) => {
       this.users = res.result;
       this.pagination = res.pagination;
